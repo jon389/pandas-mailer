40ec4504a0a46db5ebd8422a83e34dc33ba9a0d8
jon389 <24356268+jon389@users.noreply.github.com>
2021-03-10 08:59:44 +0000

    figured out formatting to render Excel looking table in Outlook

---
+++ mailer.py
@@ -0,0 +1,134 @@
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import pandas as pd
+
+
+def style_html_table(df: pd.DataFrame) -> str:
+    datetime_cols = (df.select_dtypes('datetime').columns.to_list() +
+                     [col for col in df.select_dtypes('object').columns
+                      if df[col].map(lambda x: type(x).__name__).str.contains('datetime|Timestamp').all()]
+                     )
+
+    df_html = (df
+               .style
+
+               # set text to display
+               .hide_index()
+               .format({col: '{:%a %d %b %y %H:%M:%S %Z}' for col in datetime_cols})
+               .format({col: lambda x: f'{x:,.8f}'.rstrip('0').rstrip('.')
+                        for col in df.select_dtypes('float').columns})
+
+               # CSS classes are attached to the generated HTML
+               # Index and Column names include index_name and level<k> where k is its level in a MultiIndex
+               # Index label cells include
+               #  - row_heading
+               #  - row<n> where n is the numeric position of the row
+               #  - level<k> where k is the level in a MultiIndex
+               # Column label cells include
+               #  - col_heading
+               #  - col<n> where n is the numeric position of the column
+               #  - level<k> where k is the level in a MultiIndex
+               # Blank cells include blank
+               # Data cells include data
+
+               # note that Outlook only reads the first class (currently th.col_heading and td.data)
+               # also, certain properties must be applied at lowest th/td level to render in Outlook
+               # https://tatham.blog/2009/07/05/whats-wrong-with-outlook/
+               # https://docs.microsoft.com/en-us/previous-versions/office/developer/office-2007/aa338201(v=office.12)
+
+               # set_table_styles: put css in a <style> tag before the generated HTML table
+               .set_table_styles([
+                     # non-specific table properties
+                     dict(selector='',
+                          props=[('mso-table-lspace', '0pt !important'),
+                                 ('mso-table-rspace', '0pt !important'),
+                                 ('font-family'     , 'Calibri, sans-serif'),
+                                 ('font-size'       , '11pt'),
+                                 ('color'           , 'black'),
+                                 ('text-align'      , 'left'),
+                                 ('border-collapse' , 'collapse'),
+                                 ('border'          , '1px solid #8ea9db'),  # light blue
+                                 ]),
+
+                     # table row properties (includes header), probably redundant
+                     dict(selector='tr',
+                          props=[('height', '15px')]),
+
+                     # header row, could also select with class=.col_header
+                     dict(selector='th',
+                          props=[('background', '#4472c4'),  # dark blue
+                                 ('color', 'white'),         # font color
+                                 ('font-weight', 'bold'),
+                                 ('text-align', 'left'),
+                                 ('white-space', 'nowrap'),
+                                 ('border-bottom', '1px solid #8ea9db'),  # light blue
+                                 ('padding', '2px 7px 2px 7px'),
+                                 ('height', '15px'),
+                                 ]),
+
+                     # data rows, could also select with class=.data
+                     dict(selector='td',
+                          props=[('white-space', 'nowrap'),
+                                 ('border-bottom', '1px solid #8ea9db'),  # light blue
+                                 ('padding', '2px 7px 2px 7px'),
+                                 ('height', '15px'),
+                                 ]),
+               ])
+
+               # text-align: right    # for numeric values
+
+               # add items to the opening <table> tag, legacy HTML stuff
+               .set_table_attributes('border="0" cellpadding="0" cellspacing="0"')
+
+               # .apply(), .applymap(), and .set_properties() also add css to the <style> tag
+               #  before the generated HTML table (like .set_table_styles)
+               #  but instead use #id selectors, so are compatible with Outlook
+
+               # .set_properties is for non-data dependent css style properties (just calls applymap)
+               #  slicer to select every other data row:
+               #           subset=pd.IndexSlice[df.index[::2], :]   # subset=(df.index[::2],) also works
+               .set_properties(background='#d9e1f2', subset=pd.IndexSlice[df.index[::2], :])
+               #.applymap(lambda x: 'background: #d9e1f2', subset=pd.IndexSlice[df.index[::2], :])
+
+               # right align numeric values, like Excel
+               .set_properties(**{'text-align': 'right'}, subset=df.select_dtypes('number').columns)
+               # there's no way to apply this to specific th.col_headers in Outlook, since it ignores 2nd+ classes
+               #     to right align specific th, would have to do something like adding #id to
+               #     re.sub('<th class="col_heading level0 col2">',
+               #            '<th id="col_heading_level0_col2" class="col_heading level0 col2">',
+               #            df_html)
+
+               .render()
+               )
+
+    return df_html
+
+
+def email_table(df: pd.DataFrame, smtp_svr: str, smtp_user: str, smtp_pass: str, from_email: str, to_email: str):
+    message = MIMEMultipart()
+    message['Subject'] = f'Pandas-Mailer {pd.to_datetime("now"):%Y-%m-%d %H:%M:%S}'
+    message['From'] = from_email
+    message['To'] = to_email
+
+    df_html = style_html_table(df)
+
+    # provide meta in head to suppress warning in Outlook about how message is displayed
+    msg_body = ('''
+            <!DOCTYPE html>
+            <html>
+            <head><meta name="ProgId" content="Word.Document" /></head>
+            <body>
+            ''' + df_html + '''
+            </body>
+            </html>
+            ''')
+
+    message.attach(MIMEText(msg_body, 'html'))
+    msg_body = message.as_string()
+
+    server = SMTP(smtp_svr, 587)
+    server.starttls()
+    server.login(smtp_user, smtp_pass)
+    server.sendmail(message['From'], message['To'], msg_body)
+    server.quit()
--- mkt_update.py
+++ mkt_update.py
@@ -1,5 +1,5 @@
 import pandas as pd, numpy as np
-import xlwings as xw
+# import xlwings as xw
 import yfinance as yf  # https://github.com/ranaroussi/yfinance
 
 # data = yf.download(tickers='^GSPC ES=F CL=F GBPUSD=X USDCAD=X BTC-USD', period='2d', interval='5m', prepost=True)
@@ -39,57 +39,34 @@ import yfinance as yf  # https://github.com/ranaroussi/yfinance
 def get_data() -> pd.DataFrame:
     closes = []
     for t in yf.Tickers(tickers='^GSPC ES=F CL=F GBPUSD=X USDCAD=X BTC-USD').tickers:
-        h = t.history(period='1d', interval='30m', prepost=True)
+        h = t.history(period='30m', interval='1m', prepost=True)
 
         closes.append(dict(
             **{x: t.info.get(x) for x in 'symbol shortName'.split()},
             price=round(h.Close.iloc[-1], t.info['priceHint']) if 'priceHint' in t.info else h.Close.iloc[-1],
             ccy=t.info.get('currency'),
-            at= f'{h.index[-1]:%Y-%m-%d %H:%M:%S%z(%Z)}',
+            at=h.index[-1],
             tzone=h.index[-1].tzinfo.zone if h.index[-1].tzinfo is not None else '',
 
             # _gmtOffset=int(t.info.get('gmtOffSetMilliseconds', 0))/1000/60/60,
-
         ))
 
-    return pd.DataFrame(closes)
+    return (pd.DataFrame(closes)
+              # .assign(at=lambda d: pd.to_datetime(d['at'], utc=True))
+            )
 
 
-from smtplib import SMTP
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-
-def email_table(smtp_svr:str, smtp_user:str, smtp_pass:str, from_email:str, to_email:str):
-    message = MIMEMultipart()
-    message['Subject'] = f'Pandas-Mailer {pd.to_datetime("now"):%Y-%m-%d %H:%M:%S}'
-    message['From'] = from_email
-    message['To'] = to_email
-
-    df = get_data()
-    df_html = (df.style
-                 .hide_index()
-                 .set_properties(**{'font-size': '9pt',
-                                    'font-family': 'Calibri'})
-                 .render()
-    )
-    message.attach(MIMEText(df_html, "html"))
-    msg_body = message.as_string()
-
-    server = SMTP(smtp_svr, 587)
-    server.starttls()
-    server.login(smtp_user, smtp_pass)
-    server.sendmail(message['From'], message['To'], msg_body)
-    server.quit()
-
-
-from argparse import ArgumentParser
 if __name__ == '__main__':
+    from mailer import email_table
+    def run_func(smtp_svr: str, smtp_user: str, smtp_pass: str, from_email: str, to_email: str):
+        email_table(get_data(), smtp_svr, smtp_user, smtp_pass, from_email, to_email)
+
+    from argparse import ArgumentParser
     argp = ArgumentParser()
-    run_func = email_table
     for a in run_func.__code__.co_varnames[:run_func.__code__.co_argcount]:
         argp.add_argument(a)
     run_func(**vars(argp.parse_args()))
 
 # Program/script: %ComSpec%
-# Add arguments:  /c start "" /min %ComSpec% /c "%LOCALAPPDATA%\anaconda3\condabin\conda.bat activate .\envs & python main.py smtp_svr... "
+# Add arguments:  /c start "" /min %ComSpec% /c "%LOCALAPPDATA%\anaconda3\condabin\conda.bat activate .\envs & python mkt_update.py smtp_svr... "
 # Start in: <pandas-mailer directory>
